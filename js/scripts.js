1fcf0206594ccda055b1e750b48b70c33befb44f
Birkir Brynjarsson <birkir@birkir.is>
2017-05-22 20:35:29 +0000

    initial page

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var callback = function(){
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+		$(this).animate({
+			width: newWidth,
+		}, 1000);
+	});
+	$('.icons-red').each(function(){
+		height = $(this).height();
+		$(this).animate({
+			height: 14,
+		}, 2000);
+	});
+	$('.my-age').html(getAge('May 2, 1988 00:00:01'));
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
